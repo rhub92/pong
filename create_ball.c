467a7de9fb3df69d8a43580e33868209b207f03f
Ryan Hub <c15ryan.hub@usafa.edu>
2013-10-17 19:56:45 -0600

    added a comment

--- create_ball.c
+++ create_ball.c
@@ -1,37 +1,71 @@
 #include "create_ball.h"
 
-
+/*******************************************
+Subroutine Name: createBall
+Author: 2/c Ryan Hub
+Function: assigns the ball its dimensions based off of the values passed
+Inputs: int xPos- the x-coordinate of the ball's position
+		int yPos- the y-coordinate of the ball's position
+		int xVel- the x-coordinate of the ball's velocity
+		int yVel- the y-coordinate of the ball's velocity
+		unsigned char radius- the radius of the ball
+returns: ball- the dimensions of the ball
+*******************************************/
 ball_t createBall(int xPos, int yPos, int xVel, int yVel, unsigned char radius) {
-	ball_t newball;
-	newball.location.x = xPos;
-	newball.location.y = yPos;
-	newball.velocity.x = xVel;
-	newball.velocity.y = yVel;
-	newball.radius = radius;
-	return newball;
+	ball_t ball;
+	ball.location.x = xPos;
+	ball.location.y = yPos;
+	ball.velocity.x = xVel;
+	ball.velocity.y = yVel;
+	ball.radius = radius;
+	return ball;
 }
 
 
 
-
+/*******************************************
+Subroutine Name: right_collision_detector
+Author: 2/c Ryan Hub
+Function: Determines if the ball has hit the right side of the screen
+Inputs: int xPos- the ball's x position
+		int radius- the ball's radius
+returns: char- Either TRUE or FALSE
+*******************************************/
 char right_collision_detector(int xPos, unsigned char radius) {
-	if (xPos + radius > WINDOW_WIDTH) {
+	if (xPos - radius > WINDOW_WIDTH) {
 		return TRUE;
 	} else {
 		return FALSE;
 	}
 }
 
+/*******************************************
+Subroutine Name: left_collision_detector
+Author: 2/c Ryan Hub
+Function: Determines if the ball has hit the left side of the screen
+Inputs: int xPos- the ball's x position
+		int radius- the ball's radius
+returns: char- Either TRUE or FALSE
+*******************************************/
 char left_collision_detector(int xPos, unsigned char radius) {
-	if (xPos - radius < 0) {
+	if (xPos + radius < 0) {
 		return TRUE;
 	} else {
 		return FALSE;
 	}
 }
 
+
+/*******************************************
+Subroutine Name: up_collision_detector
+Author: 2/c Ryan Hub
+Function: Determines if the ball has hit the top of the screen
+Inputs: int yPos- the ball's y position
+		int radius- the ball's radius
+returns: char- Either TRUE or FALSE
+*******************************************/
 char up_collision_detector(int yPos, unsigned char radius) {
-	if (yPos + radius > WINDOW_HEIGHT) {
+	if (yPos - radius > WINDOW_HEIGHT) {
 		return TRUE;
 	} else {
 		return FALSE;
@@ -39,8 +73,16 @@ char up_collision_detector(int yPos, unsigned char radius) {
 }
 
 
+/*******************************************
+Subroutine Name: down_collision_detector
+Author: 2/c Ryan Hub
+Function: Determines if the ball has hit the bottom of the screen
+Inputs: int yPos- the ball's y position
+		int radius- the ball's radius
+returns: char- Either TRUE or FALSE
+*******************************************/
 char down_collision_detector(int yPos, unsigned char radius) {
-	if (yPos - radius < 0) {
+	if (yPos + radius < 0) {
 		return TRUE;
 	} else {
 		return FALSE;
@@ -48,11 +90,19 @@ char down_collision_detector(int yPos, unsigned char radius) {
 
 }
 
+
+/*******************************************
+Subroutine Name: moveBall
+Author: 2/c Ryan Hub
+Function: changes the position of the ball based on its velocity
+Inputs: ball_t ball- the current dimensions of the ball
+returns: ball_t ball- the new dimensions of the ball
+*******************************************/
 ball_t moveBall(ball_t ball) {
 		ball.location.x += ball.velocity.x;
 		ball.location.y += ball.velocity.y;
 
-		if(left_collision_detector(ball.location.x, ball.radius) == 1 || right_collision_detector(ball.location.x, ball.radius) == 1) {
+		if(left_collision_detector(ball.location.x, ball.radius) == TRUE || right_collision_detector(ball.location.x, ball.radius) == TRUE) {
 			ball.velocity.x = -ball.velocity.x;
 		}
 		if(up_collision_detector(ball.location.y, ball.radius) == TRUE || down_collision_detector(ball.location.y, ball.radius) == TRUE) {
--- main.c
+++ main.c
@@ -1,10 +1,19 @@
+/*******************************************
+  * Author: 2/c Ryan Hub
+  * Created: 16 Oct 2013
+  * Description: This program replicates that of the game pong. It moves a ball around a box
+  * and when the ball hits the box the ball changes its direction, so the values of the ball's
+  * location remain in the specified box.
+*******************************************/
+
+//this is a new comment
 #include <msp430.h> 
 #include "create_ball.h"
 
 void main(void) {
     WDTCTL = WDTPW | WDTHOLD;	// Stop watchdog timer
-    vector2d_t location = {150,150};
-    vector2d_t velocity = {40,40};
+    vector2d_t location = {210,210};
+    vector2d_t velocity = {30,30};
     unsigned char radius = 5;
 
 
